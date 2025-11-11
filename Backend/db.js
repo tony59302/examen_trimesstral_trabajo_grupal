afaa29f519e556437f18659e75f96da9ab3bb8b6
jheraldy300 <sebasmosque@gmail.com>
2025-11-11 14:57:00 -0500

    Create .env.ejemplo

--- Backend/db.js
+++ Backend/db.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const useSsl = process.env.DB_SSL === 'true'; // poner DB_SSL=true en entornos que requieran TLS
+// Esta cosa sabe dar errores, si te sale alguno revisa bien la cadena de conexión en el .env
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
